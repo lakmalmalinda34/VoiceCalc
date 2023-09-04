c4f7b3ccdc9f21c56b7c58a34f9b4aad1779817d
lakmalmalinda34 <141044698+lakmalmalinda34@users.noreply.github.com>
2023-09-04 19:49:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import tempfile
 from flask import Flask, request, jsonify, send_file
 import speech_recognition as sr
 import pyttsx3
+from pydub import AudioSegment
 import re
 
 app = Flask(__name__)
@@ -40,53 +41,56 @@ def speak_text(text, output_file):
     engine.save_to_file(text, output_file)
     engine.runAndWait()
 
-@app.route('/process_voice', methods=['POST'])
+@app.route('/process_audio', methods=['POST'])
 def process_voice():
     if 'voice' not in request.files:
         return jsonify({'error': 'No file part'})
-
-    tmp_dir = 'tmp_dir'
-    os.makedirs(tmp_dir, exist_ok=True)
+    
     voice_file = request.files['voice']
-    save_path = os.path.join(tmp_dir, voice_file.filename)
-    voice_file.save(save_path)
 
-    # Check if the saved file exists
-    if not os.path.exists(save_path):
-        return jsonify({'error': 'File not saved properly'})
-
-    recognizer = sr.Recognizer()
-
-    try:
-        with sr.AudioFile(save_path) as source:
-            recognizer.adjust_for_ambient_noise(source)
-            audio = recognizer.record(source)
-
-        text = recognizer.recognize_google(audio)
+    if voice_file:
         
-        result = evaluate_math_expression(text)
-        if result is not None:
-            response_text = "The result of the calculation is: " + str(result)
-        else:
-            response_text = "Sorry, an error occurred during the calculation."
+        audio = AudioSegment.from_file(voice_file)
+        output_wav_path = "output_file.wav"
+        audio.export(output_wav_path, format="wav")
 
-        # Create a temporary WAV file to store the audio response
-        voice_clip_path = tempfile.NamedTemporaryFile(delete=False, suffix=".wav").name
-        speak_text(response_text, voice_clip_path)
+        # Check if the saved file exists
+        if not os.path.exists(output_wav_path):
+            return jsonify({'error': 'File not saved properly'})
 
-        # Remove the original uploaded voice file
-        os.remove(save_path)
+        recognizer = sr.Recognizer()
 
-        # Check if the generated audio file exists and has a non-zero size
-        if os.path.exists(voice_clip_path) and os.path.getsize(voice_clip_path) > 0:
-            # Send the generated audio file as a response
-            return send_file(voice_clip_path, mimetype='audio/wav')
-        else:
-            return jsonify({'error': 'Audio file generation failed'})
+        try:
+            with sr.AudioFile(output_wav_path) as source:
+                recognizer.adjust_for_ambient_noise(source)
+                audio = recognizer.record(source)
 
-    except Exception as e:
-        os.remove(save_path)
-        return str(e)
+            text = recognizer.recognize_google(audio)
+            
+            result = evaluate_math_expression(text)
+
+            if result is not None:
+                response_text = "The result of the calculation is: " + str(result)
+            else:
+                response_text = "Sorry, an error occurred during the calculation."
+
+            # Create a temporary WAV file to store the audio response
+            voice_clip_path = tempfile.NamedTemporaryFile(delete=False, suffix=".wav").name
+            speak_text(response_text, voice_clip_path)
+
+            # Remove the original uploaded voice file
+            os.remove(output_wav_path)
+
+            # Check if the generated audio file exists and has a non-zero size
+            if os.path.exists(voice_clip_path) and os.path.getsize(voice_clip_path) > 0:
+                # Send the generated audio file as a response
+                return send_file(voice_clip_path, mimetype='audio/wav')
+            else:
+                return jsonify({'error': 'Audio file generation failed'})
+
+        except Exception as e:
+            os.remove(output_wav_path)
+            return str(e)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
